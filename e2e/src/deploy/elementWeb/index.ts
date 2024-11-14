08d17f9d4ad90e5a537cb00088115450954d728c
Kim Brose <kim.brose@nordeck.net>
2024-11-14 19:03:52 +0100

    use latest element web and synapse releases for e2e
    Signed-off-by: Kim Brose <kim.brose@nordeck.net>

--- e2e/src/deploy/elementWeb/index.ts
+++ e2e/src/deploy/elementWeb/index.ts
@@ -22,7 +22,7 @@ let container: StartedTestContainer | undefined;
 export async function startElementWeb({
   homeserverUrl,
   widgetServerUrl,
-  version = 'v1.11.84',
+  version = 'master',
 }: {
   homeserverUrl: string;
   widgetServerUrl: string;
